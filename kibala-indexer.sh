7ca916a6eecd99aeccc4eae04662534b817600aa
Armin Pech <mail@arminpech.de>
2014-11-30 06:47:15 +0100

    kibala-indexer.sh: General index update script

---
+++ kibala-indexer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# kibala-indexer.sh 
+# Runs all tools to insert/update bacula data in elasticsearch
+
+# Abort on error
+set -e
+
+$(dirname $0)/kibala-setup-index.sh
+$(dirname $0)/kibala-import-bacula-client.sh
+$(dirname $0)/kibala-import-bacula-job.sh
+$(dirname $0)/kibala-import-bacula-pool.sh
+$(dirname $0)/kibala-import-bacula-media.sh
+$(dirname $0)/kibala-import-bacula-jobhisto.sh
