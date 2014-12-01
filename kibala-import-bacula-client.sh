599ce175fc8b1ac2da1b2dfd1ebbf010c0d4acf5
Armin Pech <mail@arminpech.de>
2014-12-01 23:07:24 +0100

    import clients: group_condat jobids, format es response

--- kibala-import-bacula-client.sh
+++ kibala-import-bacula-client.sh
@@ -10,6 +10,8 @@ mysql	--silent --raw \
 	-h$BACULA_DB_HOST \
 	-u$BACULA_DB_USERNAME \
 	$BACULA_DB_SCHEMA >/tmp/kibala-spool <<EOF
+SET SESSION group_concat_max_len = 1000000;
+
 select concat(
 	'{ "index": { "_index": "$ES_INDEX", "_type": "Client", "_id": ', c.ClientId, ' } }\n',
 	'{ "@timestamp": "', 		date_format(now(), '%Y-%m-%dT%H:%i:%s'), '"',
@@ -19,6 +21,13 @@ select concat(
 	', "ClientAutoPrune": ',	c.AutoPrune,
 	', "ClientFileRetention": ',	c.FileRetention,
 	', "ClientJobRetention": ',	c.JobRetention,
+	', "JobId": [ ',		ifnull( ( select group_concat(  DISTINCT j.JobId
+									order by j.Name separator ', ')
+						  from Job j
+						  where j.ClientId = c.ClientId
+						),
+						''
+					), ' ]',
 	', "JobName": [ ',		ifnull( ( select group_concat(  DISTINCT concat('"', j.Name, '"')
 									order by j.Name separator ', ')
 						  from Job j
@@ -31,6 +40,7 @@ select concat(
 from 	Client c;
 EOF
 
-curl -s -XPOST $ES_URL/_bulk --data-binary @/tmp/kibala-spool && rm /tmp/kibala-spool
+curl -s -XPOST $ES_URL/_bulk --data-binary @/tmp/kibala-spool | format_es_response
+rm /tmp/kibala-spool
 
 echo
