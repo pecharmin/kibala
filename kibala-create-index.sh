4a79fe0a8a7c345af21ca0a88d53b58a5b3719b4
Armin Pech <mail@arminpech.de>
2014-12-01 20:19:48 +0100

    kibala-create-index.sh: Use ES config variables

--- kibala-create-index.sh
+++ kibala-create-index.sh
@@ -5,13 +5,13 @@
 # Load configuration
 source $(dirname $0)/kibala.conf
 
-curl -XPUT $ES_URL/$ES_INDEX -d '{
-	"settings" : {
-		"index" : {
-			"number_of_shards" : 3,
-			"number_of_replicas" : 1
+curl -XPUT $ES_URL/$ES_INDEX -d "{
+	\"settings\" : {
+		\"index\" : {
+			\"number_of_shards\" : ${ELASTICSEARCH_number_of_shards:-3},
+			\"number_of_replicas\" : ${ELASTICSEARCH_number_of_replicas:-1}
 		}
 	}
-}'
+}"
 
 echo
