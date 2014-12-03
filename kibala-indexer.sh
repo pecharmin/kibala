e0b459a428c50a4a24389e10fe17bd1933d8a504
Armin Pech <mail@arminpech.de>
2014-12-03 01:05:19 +0100

    Removed unused client importer

--- kibala-indexer.sh
+++ kibala-indexer.sh
@@ -5,7 +5,6 @@
 # Abort on error
 set -e
 
-#$(dirname $0)/kibala-import-bacula-client.sh
 $(dirname $0)/kibala-import-bacula-job.sh
 $(dirname $0)/kibala-import-bacula-pool.sh
 $(dirname $0)/kibala-import-bacula-media.sh
